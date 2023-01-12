b6faddc25a860749421a279d6b4f639f2f06a6c8
daehwankim112 <kdw8731@gmail.com>
2023-01-11 23:58:34 -0800

    Tree control burning

--- Unity/Dont Extinguish/Assets/Scripts/TreeController.cs
+++ Unity/Dont Extinguish/Assets/Scripts/TreeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TreeController : MonoBehaviour
 {
@@ -11,9 +12,15 @@ public class TreeController : MonoBehaviour
     public float wateredSpeed = 10;
     public bool growing = true;
     public bool burning = false;
+    public float burningSpeed = 0.2f;
     public int damage = 20;
     public GameObject treeSample;
+    public GameObject logSample;
     public Transform treeSpawner;
+    public Transform logSpawner;
+    public TMP_Text healthText;
+
+    private float tempZeroToCalculateBurning = 0f;
 
 
     // Start is called before the first frame update
@@ -38,11 +45,28 @@ public class TreeController : MonoBehaviour
             if (burning)
             {
                 Debug.Log("Tree " + this.gameObject.name + " is burning");
+                isburning();
             }
+            if (health==100)
+            {
+                growing = false;
+            }
+            healthText.text = "" + health;
         }
         else // destroy tree
         {
-            drop( 1 );
+            if (growing && !burning) // tree is down but was still growing
+            {
+                drop(1);
+            }
+            else if (!growing && !burning) // tree is down and was fully grown
+            {
+                drop(2);
+            }
+            else if (burning) // tree was burning
+            {
+                drop(0);
+            }
             this.gameObject.SetActive(false);
             Destroy(this.gameObject, 1f);
         }
@@ -63,6 +87,30 @@ public class TreeController : MonoBehaviour
 
     public void drop( int stage )
     {
-        Instantiate(treeSample, this.gameObject.transform.position - new Vector3(0f, 1f, 0f), treeSample.transform.rotation, treeSpawner);
+        if (stage==2) // tree was fully grown
+        {
+            Instantiate(treeSample, this.gameObject.transform.position - new Vector3(0f, 1f, 0f), treeSample.transform.rotation, treeSpawner);
+            Instantiate(logSample, this.gameObject.transform.position - new Vector3(0f, 1f, 0f), logSample.transform.rotation, logSpawner);
+            Instantiate(logSample, this.gameObject.transform.position - new Vector3(0f, 1f, 0f), logSample.transform.rotation, logSpawner);
+            Instantiate(logSample, this.gameObject.transform.position - new Vector3(0f, 1f, 0f), logSample.transform.rotation, logSpawner);
+        }
+        else if (stage==1) // tree was growing
+        {
+            Instantiate(treeSample, this.gameObject.transform.position - new Vector3(0f, 1f, 0f), treeSample.transform.rotation, treeSpawner);
+        }
+        else if (stage==0) // tree was burning
+        {
+
+        }
+    }
+
+    public void isburning()
+    {
+        tempZeroToCalculateBurning += burningSpeed;
+        if(tempZeroToCalculateBurning > 1) 
+        {
+            tempZeroToCalculateBurning = 0f;
+            health -= 1;
+        }
     }
 }
