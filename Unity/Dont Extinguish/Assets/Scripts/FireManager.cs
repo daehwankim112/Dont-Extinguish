6989d1dc4370441651d9cdf288a0c8d49c58d8e8
daehwankim112 <kdw8731@gmail.com>
2023-04-20 00:52:48 -0700

    chore: fire particle lifetime and rate linked to its health "log"

--- Unity/Dont Extinguish/Assets/Scripts/FireManager.cs
+++ Unity/Dont Extinguish/Assets/Scripts/FireManager.cs
@@ -15,10 +15,37 @@ public class FireManager : MonoBehaviour
     public GameObject treeSpawner;
     public float burningRadius;
 
+    private ParticleSystem particle_magicFireProOrange;
+    private ParticleSystem particle_sparks;
+    private ParticleSystem particle_fire;
+    private ParticleSystem particle_smoke;
+    private ParticleSystem particle_fireDark;
+
+    private List<ParticleSystem> particles = new List<ParticleSystem>();
+    private List<float> particle_startLifeTime = new List<float>();
+    private List<float> particle_emissionRateOverTime = new List<float>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2;
+        speed = 10;
+        particle_magicFireProOrange = fire.gameObject.GetComponent<ParticleSystem>();
+        particle_sparks = fire.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+        particle_fire = fire.GetChild(1).gameObject.GetComponent<ParticleSystem>();
+        particle_smoke = fire.GetChild(2).gameObject.GetComponent<ParticleSystem>();
+        particle_fireDark = fire.GetChild(3).gameObject.GetComponent<ParticleSystem>();
+        Debug.Log("name of ParticleSystem: " + particle_sparks.name + ", type: " + particle_sparks.GetType() + ", name of GameObject: " + fire.GetChild(0).gameObject.name);
+        particles.Add(particle_magicFireProOrange);
+        particles.Add(particle_sparks);
+        particles.Add(particle_fire);
+        particles.Add(particle_smoke);
+        particles.Add(particle_fireDark);
+        foreach( ParticleSystem p in particles )
+        {
+            particle_startLifeTime.Add(p.main.startLifetime.constant);
+            particle_emissionRateOverTime.Add(p.emission.rateOverTime.constant);
+        }
     }
 
     // Update is called once per frame
@@ -28,7 +55,15 @@ public class FireManager : MonoBehaviour
         {
             log -= (Time.deltaTime * speed);
             Debug.Log("log: " + log);
-            fire.localScale = new Vector3(log/100, log/100, log/100);
+
+            // Updates size of Fire particles based on its health "log"
+            for ( int i = 0; i < particles.Count; i ++ )
+            {
+                ParticleSystem.MainModule tempMain = particles[i].main;
+                ParticleSystem.EmissionModule tempEmission = particles[i].emission;
+                tempMain.startLifetime = particle_startLifeTime[i] * log / 100;
+                tempEmission.rateOverTime = particle_emissionRateOverTime[i] * log / 100;
+            }
         }
         if ( GameController.reset )
         {
